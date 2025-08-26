9c9fbaf62e89734132bb184aa3bd6c1f06bb823a
Veder202320611 <vedermw@gmail.com>
2025-08-26 14:37:35 -0500

    ms_cliente

--- ms_compras/src/main/java/com/example/ms_compras/repository/ComprasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ms_compras.repository;
-
-import com.example.ms_compras.entity.Compras;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComprasRepository extends JpaRepository <Compras, Long> {
-}
