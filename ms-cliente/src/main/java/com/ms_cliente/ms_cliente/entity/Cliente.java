86b3b8515174db7e43b3fd726e81c9eae6833e43
Veder202320611 <vedermw@gmail.com>
2025-08-26 12:43:21 -0500

    ms_cliente

--- ms-cliente/src/main/java/com/ms_cliente/ms_cliente/entity/Cliente.java
+++ ms-cliente/src/main/java/com/ms_cliente/ms_cliente/entity/Cliente.java
@@ -5,14 +5,42 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+
 
 @Entity
-@Data
+
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nombre;
+
     private String descripcion;
+
+    // ===== Getters =====
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    // ===== Setters =====
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
