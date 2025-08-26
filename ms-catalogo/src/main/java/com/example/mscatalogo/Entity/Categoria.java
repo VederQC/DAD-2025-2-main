39d94e2589e6d2399e13405a1c604aec9f906237
Veder202320611 <vedermw@gmail.com>
2025-08-26 10:54:20 -0500

    arreglado

--- ms-catalogo/src/main/java/com/example/mscatalogo/Entity/Categoria.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/Entity/Categoria.java
@@ -4,16 +4,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
-@Data
+
+
 @Entity
 public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nombre;
     private String apellido;
 
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
+
+
