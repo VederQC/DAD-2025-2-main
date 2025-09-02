5126c62711baca11488ec74a163438d983144282
Veder202320611 <vedermw@gmail.com>
2025-09-02 08:55:33 -0500

    ultimo cambio compras

--- ms_compras/src/main/java/com/example/ms_compras/entity/Compras.java
+++ ms_compras/src/main/java/com/example/ms_compras/entity/Compras.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Compras {
 
@@ -17,26 +22,6 @@ public class Compras {
     private String nombre;
     private String producto;
 
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getProducto() {
-        return producto;
-    }
-    public void setProducto(String apellido) {
-        this.producto = apellido;
-    }
 }
 
 
