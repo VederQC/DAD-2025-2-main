f3c1f0515d18e352b753d013d709b9e1b1dde0f1
Veder202320611 <vedermw@gmail.com>
2025-09-29 21:13:58 -0500

    CircuitBreaker

--- ms-cliente/src/main/java/com/ms_cliente/ms_cliente/feign/CatalogoFeign.java
+++ ms-cliente/src/main/java/com/ms_cliente/ms_cliente/feign/CatalogoFeign.java
@@ -1,6 +1,7 @@
 package com.ms_cliente.ms_cliente.feign;
 
 import com.ms_cliente.ms_cliente.dto.CategoriaDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,5 +11,13 @@ import java.util.Optional;
 @FeignClient(name = "ms-catalogo", path = "/categoria")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "categoriaListarPorIdCB", fallbackMethod = "fallbackCategoria")
     public CategoriaDto buscarPorId(@PathVariable Integer id);
+
+    default CategoriaDto fallbackCategoria(Integer id, Exception e) {
+        CategoriaDto categoriaDto = new CategoriaDto();
+        categoriaDto.setId(9000000);
+        categoriaDto.setNombre("SERVICIO NO DISPONIBLE");
+        return categoriaDto;
+    }
 }
